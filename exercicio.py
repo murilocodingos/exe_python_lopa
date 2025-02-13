74176e3be88d63094fcf2f30849c5549a5fcc578
murilao python <murilo.corporativo28@gmail.com>
2025-02-12 22:45:39 -0300

    execercio concluido python

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+n1 = float(input('Insira a primeira nota:'))
+n2 = float(input('Insira a segunda nota:'))
+n3 = float(input('Insira a terceira nota:'))
+n4 = float(input('Insira a quarta nota:'))
+n5 = float(input('Insira a quinta nota:'))
+
+media = (n1+n2+n3+n4+n5) /5
+
+print(f'A média do aluno é: {media}')
